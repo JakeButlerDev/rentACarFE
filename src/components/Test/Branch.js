315ea3bf15397ed3fd7ed381e8f274fb883814a0
Jake Butler <JakeButler.student@careerdevs.com>
2023-01-28 20:56:58 -0500

    Added async function getCustomers to Test.js, added organization to project folder

--- src/components/Test/Branch.js
+++ src/components/Test/Branch.js
@@ -1,5 +1,5 @@
 import React, {useState} from "react";
-import Button from '..common/common/Button';
+import Button from '..common/Button';
 
 const Branch = () => {
     
--- src/components/Test/Test.js
+++ src/components/Test/Test.js
@@ -1,13 +1,23 @@
-import React, {useState} from 'react';
-import Button from '../common/common/Button';
+import React, {useState, useEffect} from 'react';
+import Button from '../common/Button';
 
 const Test = () => {
 
-    const [counter, setCounter] = useState(0)
+    const [counter, setCounter] = useState(0);
+    const [customer, setCustomer] = useState({});
+
 
     const increaseCounter = () =>{
         setCounter(counter+1);
     }
+
+    useEffect(() => {
+        const getCustomers = async() => {
+            const res = await axios.get("localhost:5253/api/customers/");
+            console.log(res.data);
+        }
+        getCustomers();
+    }, [])
 
     return (
         <div>
