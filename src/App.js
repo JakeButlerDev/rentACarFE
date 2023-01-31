0922a4c192bd71673b1764ec95d5324d4929cf65
Jake Butler <JakeButler.student@careerdevs.com>
2023-01-30 23:01:35 -0500

    Added Branch

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Test from './components/Test/Test';
+import Branch from './components/Test/Branch';
 
 function App() {
   return (
     <div className="App">
       <Test />
+      <Branch />
     </div>
   );
 }
